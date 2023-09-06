48319067f569be8cbf5926a6026cb65f090f7c54
Phạm Thị Mai <pham82755@gmail.com>
2023-09-06 14:23:13 +0700

    update news tour page 95%

--- assets/js/detail-room.js
+++ assets/js/detail-room.js
@@ -8,3 +8,21 @@ function clickHotel(smallImg)
     var fullImg = document.getElementById("imageBoxHotel1");
     fullImg.src = smallImg.src;
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const sidebar = document.querySelector(".box.booking-room-ticket");
+    let isFixed = false;
+    const sidebarTop = sidebar.getBoundingClientRect().top;
+    console.log(sidebarTop);
+
+    window.addEventListener("scroll", function() {
+        const scrollY = window.scrollY;
+
+        if (scrollY >= sidebarTop && !isFixed) {
+            sidebar.classList.add("fixed");
+            isFixed = true;
+        } else if (scrollY < sidebarTop && isFixed) {
+            sidebar.classList.remove("fixed");
+            isFixed = false;
+        }
+    });
+});
